8fbc7b6ba610fb467f1eff8a1b02e07b6266164d
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-10-20 15:02:24 +0600

    featured cars

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import FeaturedCars from "../components/home/FeaturedCars";
 import HeroSection from "../components/home/HeroSection";
 
 const Home = () => {
   return (
     <div className="">
       <HeroSection />
+      <FeaturedCars />
     </div>
   );
 };
